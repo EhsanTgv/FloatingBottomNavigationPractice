b19b45f9c271a2a197384a222ecb5bc05708b72b
EhsanTgv <EhsanTgv@gmail.com>
2023-01-22 23:53:48 +0300

    Implement the design

--- app/src/main/java/com/taghavi/floatingbottomnavigationpractice/MainActivity.kt
+++ app/src/main/java/com/taghavi/floatingbottomnavigationpractice/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.taghavi.floatingbottomnavigationpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.taghavi.floatingbottomnavigationpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            Toast.makeText(this@MainActivity, item.title, Toast.LENGTH_SHORT).show()
+            true
+        }
     }
 }
